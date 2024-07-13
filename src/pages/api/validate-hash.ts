6547da5be8ff5693f9e6f3aa6adc8408e381905b
rabiu <medrabiu@gmail.com>
2024-07-13 23:02:45 +0100

    new

--- src/pages/api/validate-hash.ts
+++ src/pages/api/validate-hash.ts
@@ -1,31 +1,38 @@
-import { webcrypto } from 'crypto';
+import crypto from 'crypto';
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+const { webcrypto } = crypto;
 
 type Data = { ok: boolean } | { error: string };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+  try {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ error: 'Method not allowed' });
+    }
 
-  if (!req.body.hash) {
-    return res.status(400).json({
-      error: 'Missing required field hash',
-    });
-  }
+    if (!req.body.hash) {
+      return res.status(400).json({ error: 'Missing required field hash' });
+    }
 
-  if (!process.env.BOT_TOKEN) {
+    if (!process.env.BOT_TOKEN) {
+      return res.status(500).json({ error: 'Internal server error: Missing BOT_TOKEN' });
+    }
+
+    const data = Object.fromEntries(new URLSearchParams(req.body.hash));
+    console.log('Data:', data);
+
+    const isValid = await isHashValid(data, process.env.BOT_TOKEN);
+
+    if (isValid) {
+      return res.status(200).json({ ok: true });
+    }
+
+    return res.status(403).json({ error: 'Invalid hash' });
+  } catch (error) {
+    console.error('Error validating hash:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
-
-  const data = Object.fromEntries(new URLSearchParams(req.body.hash));
-  const isValid = await isHashValid(data, process.env.BOT_TOKEN);
-
-  if (isValid) {
-    return res.status(200).json({ ok: true });
-  }
-
-  return res.status(403).json({ error: 'Invalid hash' });
 }
 
 async function isHashValid(data: Record<string, string>, botToken: string) {
@@ -36,6 +43,8 @@ async function isHashValid(data: Record<string, string>, botToken: string) {
     .map((key) => `${key}=${data[key]}`)
     .sort()
     .join('\n');
+
+  console.log('CheckString:', checkString);
 
   const secretKey = await webcrypto.subtle.importKey(
     'raw',
@@ -58,6 +67,8 @@ async function isHashValid(data: Record<string, string>, botToken: string) {
   const signature = await webcrypto.subtle.sign('HMAC', signatureKey, encoder.encode(checkString));
 
   const hex = Buffer.from(signature).toString('hex');
+
+  console.log('Generated Hash:', hex);
 
   return data.hash === hex;
 }
